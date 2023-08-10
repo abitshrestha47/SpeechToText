39b393f2cf1312e0b97964530a4158f10b7c8dd7
abitshrestha <abitshrestha2079@gmail.com>
2023-08-10 12:57:57 +0545

    stopping

--- front-end/src/components/UsrInputs.js
+++ front-end/src/components/UsrInputs.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import SpeechRecognition,{useSpeechRecognition} from 'react-speech-recognition';
 
 const UsrInputs=()=>{
-    const startListening=()=>SpeechRecognition.startListening({continuous:true,language:'en'});
+    const [isListening,setisListening]=useState(false);
+    const copytoClipboard=()=>{
+        navigator.clipboard.write(transcript)
+        .then(()=>{
+            alert('Copied to clipboard');
+        })
+        .catch(error=>{
+            console.log(`error copying to clipboard: ${error}`);
+        })
+    }
+    const startListening=()=>{
+        SpeechRecognition.startListening({continuous:true,language:'en'});
+        setisListening(true);
+    }
+    const stopListening=()=>{
+        SpeechRecognition.stopListening();
+        setisListening(false);
+    }
     const {transcript,browserSupportsSpeechRecognition}=useSpeechRecognition();
     if(!browserSupportsSpeechRecognition){
         return null;
@@ -21,10 +38,11 @@ const UsrInputs=()=>{
             <h1>Speech To Text</h1>
             <div className='saySomething'>
                 <label>Say Something . . .</label>
-                <button type="button" onClick={startListening} className='usrButton'> <i className="material-icons">mic</i></button>
+                {isListening?<button type="button" onClick={stopListening} className='usrButton'><i className="material-icons">mic</i></button>:<button type="button" onClick={startListening} className='usrButton'><i className="material-icons">mic_off</i></button>}
             </div>
            
-            <input type="text" value={transcript} placeholder='Your text will be shown here...'/>
+            <textarea rows={10} cols={20} value={transcript} placeholder='Your text will be shown here...'/>
+            <button type='button' class="copyBtn" onClick={copytoClipboard}>Copy</button>
         </div>
     )
 }
